8b4ce5bd1becb9927537bffa62990787e3f9619b
Daniel Ramyar <DanielRamyar@gmail.com>
2019-05-31 15:38:34 +0200

    Added new 2 bit decoder implementation to maken bit decoder easier to implement

---
+++ SME_Implementations/Decoder_2_Bit_new_implementation/Buses.cs
@@ -0,0 +1,51 @@
+using System;
+using SME;
+
+namespace Decoder_2_Bit {
+    [TopLevelInputBus]
+    public interface Input_0 : IBus {
+        [InitialValue]
+        bool Value {get; set;}
+    }
+
+    public interface Input_1 : IBus {
+        [InitialValue]
+        bool Value {get; set;}
+    }
+
+    [TopLevelOutputBus]
+    public interface NOTOutput_0 : IBus {
+        [InitialValue]
+        bool Value {get; set;}
+    }
+
+    [TopLevelOutputBus]
+    public interface NOTOutput_1 : IBus {
+        [InitialValue]
+        bool Value {get; set;}
+    }
+
+    [TopLevelOutputBus]
+    public interface ANDOutput_0 : IBus {
+        [InitialValue]
+        bool Value {get; set;}
+    }
+
+    [TopLevelOutputBus]
+    public interface ANDOutput_1 : IBus {
+        [InitialValue]
+        bool Value {get; set;}
+    }
+
+    [TopLevelOutputBus]
+    public interface ANDOutput_2 : IBus {
+        [InitialValue]
+        bool Value {get; set;}
+    }
+
+    [TopLevelOutputBus]
+    public interface ANDOutput_3 : IBus {
+        [InitialValue]
+        bool Value {get; set;}
+    }
+}
---
+++ SME_Implementations/Decoder_2_Bit_new_implementation/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using SME;
+
+namespace Decoder_2_Bit {
+    class Program {
+        static void Main(string[] args) {
+            using(var sim = new Simulation()) {
+
+                var simulator = new DecoderSimulator();
+                var NOTGate_0 = new NOTGate_0();
+                var NOTGate_1 = new NOTGate_1();
+
+                var ANDGate_0 = new ANDGate_0();
+                var ANDGate_1 = new ANDGate_1();
+                var ANDGate_2 = new ANDGate_2();
+                var ANDGate_3 = new ANDGate_3();
+
+                sim
+                    .BuildCSVFile()
+                    .BuildGraph()
+                    .BuildVHDL()
+    			    .Run();
+            }
+        }
+    }
+}
