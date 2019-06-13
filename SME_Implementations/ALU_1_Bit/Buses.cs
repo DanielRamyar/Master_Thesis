0cc455f7d117e0999f24e7790aad154e6c74b793
Daniel Ramyar <DanielRamyar@gmail.com>
2019-06-13 17:24:04 +0200

    Added buses to 1 bit ALU

--- SME_Implementations/ALU_1_Bit/Buses.cs
+++ SME_Implementations/ALU_1_Bit/Buses.cs
@@ -15,80 +15,15 @@ namespace ALU_1_Bit {
     }
 
     [TopLevelInputBus]
-    public interface CarryIn : IBus {
+    public interface OperationCode : IBus {
         [InitialValue]
         bool Value {get; set;}
     }
 
     [TopLevelOutputBus]
-    public interface NOT_A : IBus {
+    public interface Output : IBus {
         [InitialValue]
         bool Value {get; set;}
     }
 
-    [TopLevelOutputBus]
-    public interface NOT_B : IBus {
-        [InitialValue]
-        bool Value {get; set;}
-    }
-
-    [TopLevelOutputBus]
-    public interface NOT_CarryIn : IBus {
-        [InitialValue]
-        bool Value {get; set;}
-    }
-
-    [TopLevelOutputBus]
-    public interface ANDOutput_0 : IBus {
-        [InitialValue]
-        bool Value {get; set;}
-    }
-
-    [TopLevelOutputBus]
-    public interface ANDOutput_1 : IBus {
-        [InitialValue]
-        bool Value {get; set;}
-    }
-
-    [TopLevelOutputBus]
-    public interface ANDOutput_2 : IBus {
-        [InitialValue]
-        bool Value {get; set;}
-    }
-
-    [TopLevelOutputBus]
-    public interface ANDOutput_3 : IBus {
-        [InitialValue]
-        bool Value {get; set;}
-    }
-
-    [TopLevelOutputBus]
-    public interface ANDOutput_4 : IBus {
-        [InitialValue]
-        bool Value {get; set;}
-    }
-
-    [TopLevelOutputBus]
-    public interface ANDOutput_5 : IBus {
-        [InitialValue]
-        bool Value {get; set;}
-    }
-
-    [TopLevelOutputBus]
-    public interface ANDOutput_6 : IBus {
-        [InitialValue]
-        bool Value {get; set;}
-    }
-
-    [TopLevelOutputBus]
-    public interface SUM : IBus {
-        [InitialValue]
-        bool Value {get; set;}
-    }
-
-    [TopLevelOutputBus]
-    public interface CarryOut : IBus {
-        [InitialValue]
-        bool Value {get; set;}
-    }
 }
