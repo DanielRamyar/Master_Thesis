900b560e412fb5b9145d15637f572134d445c826
Daniel Ramyar <DanielRamyar@gmail.com>
2019-09-16 14:14:44 +0200

    now switches acording to control input

--- SME_Implementations/SingleCycleRISCV/Inc_mux.cs
+++ SME_Implementations/SingleCycleRISCV/Inc_mux.cs
@@ -6,17 +6,21 @@ namespace SingleCycleRISCV {
     public class Inc_mux : SimpleProcess {
         [InputBus]
         private readonly Incrementer_Output m_input = Scope.CreateOrLoadBus<Incrementer_Output>();
+        [InputBus]
+        private readonly Zero_out m_zero_out = Scope.CreateOrLoadBus<Zero_out>();
+        [InputBus]
+        private readonly Branch m_Branch = Scope.CreateOrLoadBus<Branch>();
         [OutputBus]
         public readonly PC_Input Mux_out = Scope.CreateOrLoadBus<PC_Input>();
 
 
         protected override void OnTick() {
-            byte mux_opcode = 0;
+            bool mux_opcode = m_zero_out.Value && m_Branch.Enable; // This is the AND Gate 
             switch (mux_opcode) {
-                case 0:
+                case false:
                     Mux_out.Address = m_input.Address; 
                     break;
-                case 1:
+                case true:
                     
                     break;
 
