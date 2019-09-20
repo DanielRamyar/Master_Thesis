564235fb89842c7255756c8c740de6af7ef9866c
Daniel Ramyar <DanielRamyar@gmail.com>
2019-09-20 11:48:09 +0200

    can now select immgen out also

--- SME_Implementations/SingleCycleRISCV/Reg_mux.cs
+++ SME_Implementations/SingleCycleRISCV/Reg_mux.cs
@@ -7,6 +7,9 @@ namespace SingleCycleRISCV {
         private readonly Reg2_To_Mux m_Reg_in = Scope.CreateOrLoadBus<Reg2_To_Mux>();
         [InputBus]
         private readonly ALUSrc m_ALUSrc = Scope.CreateOrLoadBus<ALUSrc>();
+        [InputBus]
+        private readonly ImmGen_Out m_ImmGen_in = Scope.CreateOrLoadBus<ImmGen_Out>();
+
 
         [OutputBus]
         public readonly Reg_Mux_Output Mux_out = Scope.CreateOrLoadBus<Reg_Mux_Output>();
@@ -18,7 +21,7 @@ namespace SingleCycleRISCV {
                     Mux_out.Data = m_Reg_in.Data; 
                     break;
                 case true:
-                    
+                    Mux_out.Data = m_ImmGen_in.Immediate;
                     break;
 
             }
