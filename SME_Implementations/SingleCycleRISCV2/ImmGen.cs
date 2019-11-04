0f11dd8672ca46f4dfb1fdc1df028d890730e87b
Daniel Ramyar <DanielRamyar@gmail.com>
2019-11-04 19:38:46 +0100

    added non complete immgen unit

---
+++ SME_Implementations/SingleCycleRISCV2/ImmGen.cs
@@ -0,0 +1,54 @@
+using System;
+using SME;
+
+namespace SingleCycleRISCV {
+    public interface ImmGen_Out : IBus {
+        [InitialValue]
+        long Immediate {get; set;}
+    }
+
+    public class ImmGen : SimpleProcess {
+        [InputBus]
+        private readonly Instruction m_instruction = Scope.CreateOrLoadBus<Instruction>();
+        
+        [OutputBus]
+		public readonly ImmGen_Out output = Scope.CreateOrLoadBus<ImmGen_Out>();
+
+        long temp0;
+        uint temp1, temp2, temp3, temp4;
+
+        protected override void OnTick() {
+            uint opcode = m_instruction.Current & (uint)127; // Extracts opcode from current instruction
+
+            // Checks if opcode is a B-type branching instruction
+            if (opcode == 99) { 
+                temp1 = m_instruction.Current >> 8   & (uint)15;  // Extracts bit 8-11 from instruction
+                temp2 = m_instruction.Current >> 25  & (uint)63;  // Extracts bit 25-30
+                temp3 = m_instruction.Current >> 7   & (uint)1;   // Extracts bit 7 from instruction
+                temp4 = m_instruction.Current >> 31  & (uint)1;   // Extracts bit 11
+
+                // Creates signextended immediate field for B-type instruction
+                temp0 = (long)0 | (long)temp4 << 11
+                                | (long)temp3 << 10
+                                | (long)temp2 << 4
+                                | (long)temp1;
+
+                output.Immediate = temp0;
+            }
+            // Checks if opcode is a S-type instruction
+            if (opcode == 35) { 
+                temp1 = m_instruction.Current >> 7   & (uint)31;   // Extracts bit 7-11 from instruction
+                temp2 = m_instruction.Current >> 25  & (uint)127;  // Extracts bit 25-31 from instruction
+
+                // Creates signextended immediate field for S-type instruction
+                temp0 = (long)0 | (long)temp2 << 5
+                                | (long)temp1;
+
+                output.Immediate = temp0;
+            }
+
+
+        }
+    }
+
+}
