bf40d7c03d1837ac2bb9fa71166301d620afe6fb
Daniel Ramyar <DanielRamyar@gmail.com>
2019-11-07 17:23:00 +0100

    changed all nested if/else to switch statements

--- SME_Implementations/SingleCycleRISCV2/Control.cs
+++ SME_Implementations/SingleCycleRISCV2/Control.cs
@@ -100,35 +100,46 @@ namespace SingleCycleRISCV {
                     MemRead.Enable = false;
                     WBSel.Value = 0;
 
-                    if (funct3 == 0 && funct7 == 0) {         // ADD
-                        ALUOP.Value = 0;
-                    }
-                    else if (funct3 == 0 && funct7 == 32) {   // Subtract
-                        ALUOP.Value = 1;
-                    }
-                    else if (funct3 == 7 && funct7 == 0) {    // AND
-                        ALUOP.Value = 2;
-                    }
-                    else if (funct3 == 6 && funct7 == 0) {    // OR
-                        ALUOP.Value = 3;
-                    }
-                    else if (funct3 == 4 && funct7 == 0) {    // XOR
-                        ALUOP.Value = 4;
-                    }
-                    else if (funct3 == 1 && funct7 == 0) {    // SLL
-                        ALUOP.Value = 5;
-                    }
-                    else if (funct3 == 2 && funct7 == 0) {    // SLT
-                        ALUOP.Value = 6;
-                    }
-                    else if (funct3 == 3 && funct7 == 0) {    // SLT (U)
-                        ALUOP.Value = 7;
-                    }
-                    else if (funct3 == 5 && funct7 == 0) {    // SRL
-                        ALUOP.Value = 8;
-                    }
-                    else if (funct3 == 5 && funct7 == 32) {   // SRA
-                        ALUOP.Value = 9;
+                    switch (funct3) {
+                        case 0:
+                            switch (funct7) {
+                                case 0:                         // ADD
+                                    ALUOP.Value = 0;
+                                    break;
+                                case 32:                        // Subtract
+                                    ALUOP.Value = 1;
+                                    break;
+                            }
+                            break;
+                        case 7:                                 // AND
+                            ALUOP.Value = 2;
+                            break;
+                        case 6:                                 // OR
+                            ALUOP.Value = 3;
+                            break;
+                        case 4:                                 // XOR
+                            ALUOP.Value = 4;
+                            break;
+                        case 1:                                 // SLL
+                            ALUOP.Value = 5;
+                            break;
+                        case 2:                                 // SLT
+                            ALUOP.Value = 6;
+                            break;
+                        case 3:                                 // SLT (U)
+                            ALUOP.Value = 7;
+                            break;
+                        case 5:
+                            switch (funct7) {
+                                case 0:                         //SRL
+                                    ALUOP.Value = 8;
+                                    break;
+                                case 32:                        //SRA
+                                    ALUOP.Value = 9;
+                                    break;
+                            }
+                            break;
+                        
                     }
                     break;
                 case 59:                                        // R-format Word
@@ -142,20 +153,30 @@ namespace SingleCycleRISCV {
                     MemRead.Enable = false;
                     WBSel.Value = 0;
 
-                    if (funct3 == 0 && funct7 == 0) {         // ADD Word
-                        ALUOP.Value = 10;
-                    }
-                    else if (funct3 == 0 && funct7 == 32) {   // Subtract Word
-                        ALUOP.Value = 11;
-                    }
-                    else if (funct3 == 1 && funct7 == 0) {    // SLL Word
-                        ALUOP.Value = 12;
-                    }
-                    else if (funct3 == 5 && funct7 == 0) {    // SRL Word
-                        ALUOP.Value = 13;
-                    }
-                    else if (funct3 == 5 && funct7 == 32) {   // SRA Word
-                        ALUOP.Value = 14;
+                    switch (funct3) {
+                        case 0:
+                            switch (funct7) {
+                                case 0:                         // ADD Word
+                                    ALUOP.Value = 10;
+                                    break;
+                                case 32:                        // Subtract Word
+                                    ALUOP.Value = 11;
+                                    break;
+                            }
+                            break;
+                        case 1:                                 //SLL
+                            ALUOP.Value = 12;
+                            break;
+                        case 5:
+                            switch (funct7) {
+                                case 0:                         // SRL Word
+                                    ALUOP.Value = 13;
+                                    break;
+                                case 32:                        // SRA Word
+                                    ALUOP.Value = 14;
+                                    break;
+                            }
+                            break;
                     }
                     break;
                 case 19:                                        // I-format
@@ -169,32 +190,38 @@ namespace SingleCycleRISCV {
                     MemRead.Enable = false;
                     WBSel.Value = 0;
 
-                    if (funct3 == 0) {                        // ADDI
-                        ALUOP.Value = 0;
-                    }
-                    else if (funct3 == 7) {                   // ANDI
-                        ALUOP.Value = 2;
-                    }
-                    else if (funct3 == 6) {                   // ORI
-                        ALUOP.Value = 3;
-                    }
-                    else if (funct3 == 4) {                   // XORI
-                        ALUOP.Value = 4;
-                    }
-                    else if (funct3 == 1 && funct7 == 0) {    // SLLI
-                        ALUOP.Value = 5;
-                    }
-                    else if (funct3 == 2) {                   // SLTI
-                        ALUOP.Value = 6;
-                    }
-                    else if (funct3 == 3) {                   // SLT (U)
-                        ALUOP.Value = 7;
-                    }
-                    else if (funct3 == 5 && funct7 == 0) {    // SRLI
-                        ALUOP.Value = 8;
-                    }
-                    else if (funct3 == 5 && funct7 == 32) {   // SRAI
-                        ALUOP.Value = 9;
+                    switch (funct3) {
+                        case 0:                                 // ADDI                
+                            ALUOP.Value = 0;
+                            break;
+                        case 7:                                 // ANDI
+                            ALUOP.Value = 2;
+                            break;
+                        case 6:                                 // ORI
+                            ALUOP.Value = 3;
+                            break;
+                        case 4:                                 // XORI
+                            ALUOP.Value = 4;
+                            break;
+                        case 1:                                 // SLLI
+                            ALUOP.Value = 5;
+                            break;
+                        case 2:                                 // SLTI
+                            ALUOP.Value = 6;
+                            break;
+                        case 3:                                 // SLTI (U)
+                            ALUOP.Value = 7;
+                            break;
+                        case 5:
+                            switch (funct7) {
+                                case 0:                         //SRLI
+                                    ALUOP.Value = 8;
+                                    break;
+                                case 32:                        //SRAI
+                                    ALUOP.Value = 9;
+                                    break;
+                            }
+                            break;
                     }
                     break;
                 case 27:                                        // I-format Word
@@ -208,17 +235,23 @@ namespace SingleCycleRISCV {
                     MemRead.Enable = false;
                     WBSel.Value = 0;
 
-                    if (funct3 == 0) {                        // ADDI Word
-                        ALUOP.Value = 10;
-                    }
-                    else if (funct3 == 1 && funct7 == 0) {    // SLLI Word
-                        ALUOP.Value = 12;
-                    }
-                    else if (funct3 == 5 && funct7 == 0) {    // SRLI Word
-                        ALUOP.Value = 13;
-                    }
-                    else if (funct3 == 5 && funct7 == 32) {   // SRAI Word
-                        ALUOP.Value = 14;
+                    switch (funct3) {
+                        case 0:                                 // ADDI Word
+                            ALUOP.Value = 10;
+                            break;
+                        case 1:                                 // SLLI
+                            ALUOP.Value = 12;
+                            break;
+                        case 5:
+                            switch (funct7) {
+                                case 0:                         // SRLI Word
+                                    ALUOP.Value = 13;
+                                    break;
+                                case 32:                        // SRAI Word
+                                    ALUOP.Value = 14;
+                                    break;
+                            }
+                            break;
                     }
                     break;
                 case 3:                                         // LOAD
@@ -232,26 +265,28 @@ namespace SingleCycleRISCV {
                     MemRead.Enable = true;
                     WBSel.Value = 1;
 
-                    if (funct3 == 0) {                        // LOAD Byte
-                        SizeAndSign.Value = 3;
-                    }
-                    else if (funct3 == 1) {                   // LOAD Half
-                        SizeAndSign.Value = 2;
-                    }
-                    else if (funct3 == 2) {                   // LOAD Word
-                        SizeAndSign.Value = 1;
-                    }
-                    else if (funct3 == 3) {                   // LOAD Double
-                        SizeAndSign.Value = 0;
-                    }
-                    else if (funct3 == 4) {                   // LOAD Byte (U)
-                        SizeAndSign.Value = 4;
-                    }
-                    else if (funct3 == 5) {                   // LOAD Half (U)
-                        SizeAndSign.Value = 5;
-                    }
-                    else if (funct3 == 6) {                   // LOAD Word (U)
-                        SizeAndSign.Value = 6;
+                    switch (funct3) {
+                        case 0:                                 // LOAD Byte
+                            SizeAndSign.Value = 3;
+                            break;
+                        case 1:                                 // LOAD Half
+                            SizeAndSign.Value = 2;
+                            break;
+                        case 2:                                 // LOAD Word
+                            SizeAndSign.Value = 1;
+                            break;
+                        case 3:                                 // LOAD Double
+                            SizeAndSign.Value = 0;
+                            break;
+                        case 4:                                 // LOAD Byte (U)
+                            SizeAndSign.Value = 4;
+                            break;
+                        case 5:                                 // LOAD Half (U)
+                            SizeAndSign.Value = 5;
+                            break;
+                        case 6:                                 // LOAD Word (U)
+                            SizeAndSign.Value = 6;
+                            break;
                     }
                     break;
                 case 103:                                       // JALR
@@ -277,17 +312,19 @@ namespace SingleCycleRISCV {
                     MemRead.Enable = false;
                     WBSel.Value = 0;
 
-                    if (funct3 == 0) {                        // Store Byte
-                        SizeAndSign.Value = 3;
-                    }
-                    else if (funct3 == 1) {                   // Store Half
-                        SizeAndSign.Value = 2;
-                    }
-                    else if (funct3 == 2) {                   // Store Word
-                        SizeAndSign.Value = 1;
-                    }
-                    else if (funct3 == 3) {                   // Store Double
-                        SizeAndSign.Value = 0;
+                    switch (funct3) {
+                        case 0:                                 // Store Byte
+                            SizeAndSign.Value = 3;
+                            break;
+                        case 1:                                 // Store Half
+                            SizeAndSign.Value = 2;
+                            break;
+                        case 2:                                 // Store Word
+                            SizeAndSign.Value = 1;
+                            break;
+                        case 3:                                 // Store Double
+                            SizeAndSign.Value = 0;
+                            break;
                     }
                     break;
                 case 99:                                        // B-format
@@ -301,23 +338,25 @@ namespace SingleCycleRISCV {
                     MemRead.Enable = false;
                     WBSel.Value = 0;
 
-                    if (funct3 == 0) {                        // BEQ
-                        BJSIGN.Value = 0;
-                    }
-                    else if (funct3 == 1) {                   // BNE
-                        BJSIGN.Value = 1;
-                    }
-                    else if (funct3 == 4) {                   // BLT
-                        BJSIGN.Value = 2;
-                    }
-                    else if (funct3 == 5) {                   // BGE
-                        BJSIGN.Value = 3;
-                    }
-                    else if (funct3 == 6) {                   // BLTU
-                        BJSIGN.Value = 4;
-                    }
-                    else if (funct3 == 7) {                   // BGEU
-                        BJSIGN.Value = 5;
+                    switch (funct3) {
+                        case 0:                                 // LOAD Byte
+                            BJSIGN.Value = 0;
+                            break;
+                        case 1:                                 // LOAD Half
+                            BJSIGN.Value = 1;
+                            break;
+                        case 4:                                 // LOAD Word
+                            BJSIGN.Value = 2;
+                            break;
+                        case 5:                                 // LOAD Double
+                            BJSIGN.Value = 3;
+                            break;
+                        case 6:                                 // LOAD Byte (U)
+                            BJSIGN.Value = 4;
+                            break;
+                        case 7:                                 // LOAD Half (U)
+                            BJSIGN.Value = 5;
+                            break;
                     }
                     break;
                 case 23:                                        // AUIPC
