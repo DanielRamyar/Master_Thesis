59b5d2e33dc5d75cb2e5684eca93d7bb87062b2c
Daniel Ramyar <DanielRamyar@gmail.com>
2020-04-13 19:54:34 +0200

    test

--- SME_Implementations/SingleCycleRISCV2/Instruction_Memory.cs
+++ SME_Implementations/SingleCycleRISCV2/Instruction_Memory.cs
@@ -41,7 +41,7 @@ namespace SingleCycleRISCV {
         private readonly byte[] Instruction_Memory = System.IO.File.ReadAllBytes("/Users/danielramyar/Desktop/Thesis/SME_Implementations/SingleCycleRISCV2/RISCVTESTERS/fibonacci/fibowitharray.bin");      
         // private readonly byte[] Instruction_Memory = {0x00, 0x80, 0x08, 0x13,       // addi x16, x0, 8
         //                                               0x00, 0x70, 0x08, 0x93,       // addi x17, x0, 7
-        //                                               0x01, 0x08, 0x89, 0x33};      // add x18, x17, x1     
+        //                                               0x01, 0x08, 0x89, 0x33};      // add x18, x17, x16      
 
         protected override void OnTick() {
             ulong temp_address = m_input.Address;
