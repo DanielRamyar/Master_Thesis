a8c29e1c43280ca1b1b749f533e86fb93ba7bd90
Daniel Ramyar <DanielRamyar@gmail.com>
2020-04-14 20:34:30 +0200

    added image segmentation test

--- random/chi2/chi2/main.cpp
+++ random/chi2/chi2/main.cpp
@@ -8,22 +8,16 @@
 
 #include <iostream>
 
-double sqrt(double a) {
-    return a*a;
-}
-
 int main(int argc, const char * argv[]) {
     
     // Array declaration by initializing elements
     int observed[] = {30, 14, 34, 45, 57, 20};
     int expected[] = {20, 20, 30, 40, 60, 30};
     double chi2 = 0;
-    double temp;
     
     // Calculate chi2
     for (int i=0; i<6; i++) {
-        temp = observed[i]-expected[i];
-        chi2 += sqrt(temp) / expected[i];
+        chi2 = observed[i]-expected[i];
     }
     
     printf("%f\n",chi2);
